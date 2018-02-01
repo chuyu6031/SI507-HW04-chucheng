21bddff7b33588e772361897f7e76a9a75c64b86
Luke <luketk@umich.edu>
2018-02-01 14:53:43 -0500

    branch c made

--- magic_eight.py
+++ magic_eight.py
@@ -1,8 +1,9 @@
+import random
+
 def question():
 	answer = input("What is your question?")
 
-def anotherfunction():
-	pass
-
-def tryagain():
-	pass
+def pick_answer():
+	ten_answersC = ["Reply hazy try again","Ask again later","Better not tell you now","Cannot predict now","Concentrate and ask again", 
+					"Don't count on it","My reply is no","My sources say no","Outlook not so good","Very doubtful"]
+	return(random.choice(ten_answersC))
